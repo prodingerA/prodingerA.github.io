f62e4353f657babed2400c7f94c9696c9c061d50
André Prodinger <prodinger.andre@gmail.com>
2020-04-26 21:18:23 +0200

    Adding DarkMode

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,10 +2,40 @@ $(document).ready(function(){
     $(".animated").addClass("delay-1s");
     $('input[type="checkbox"]').click(function(){
         if($(this).is(":checked")){
-            $("#canvas-wrapper").css("background", "#222");
+            $('.bg-white').addClass('bg-dark-dark');
+            $('.navbar-white').addClass('navbar-dark-dark');
+            $('.box').addClass('box-dark');
+            $('.white-text').addClass('text-dark-dark');
+            $('.black-text').addClass('text-dark-dark');
+            $('#intro').addClass('intro-dark');
+            $('body').addClass('dark-dark');
+            $('h1,h2,h3,h4,h5,p,a,em,i').addClass('text-dark-dark');
+            $('.alert-info').addClass('alert-success');
+            $('.needto').addClass('perm');
+            $('.alert-info').removeClass('alert-info');
+            $('.cont').addClass('cont-dark');
+            $('#about_sectionbreak').addClass('about_sectionbreak-dark');
+            $('.btn-danger').addClass('btn-success');
+            $('.card').addClass('card-dark');
+           
+            
         }
         else if($(this).is(":not(:checked)")){
-         
+            $('.bg-white').removeClass('bg-dark-dark');
+            $('.navbar-white').removeClass('navbar-dark-dark');
+            $('.box').removeClass('box-dark');
+            $('.white-text').removeClass('text-dark-dark');
+            $('.black-text').removeClass('text-dark-dark');
+            $('#intro').removeClass('intro-dark');
+            $('body').removeClass('dark-dark');
+            $('h1,h2,h3,h4,h5,p,a,em,i').removeClass('text-dark-dark');
+            $('.alert-success').removeClass('alert-success');
+            $('.needto').removeClass('perm');
+            $('.cont').removeClass('cont-dark');
+            $('#about_sectionbreak').removeClass('about_sectionbreak-dark');
+            $('.btn-danger').removeClass('btn-success');
+            $('.card').removeClass('card-dark');
+            $('.alert').addClass('alert-info');
         }
     });
     
